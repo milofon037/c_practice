2f6d93e33e9af13b0faf9dcaebc2e0b44c472afe
Ruslan Safaroff <62283143+NimblePython@users.noreply.github.com>
2025-02-14 21:08:09 +0300

    Переделай задачи с динамическим массивом
    Я исправил одну задачу, надо переделать остальные по подобию...

--- hw_pr2/2.c
+++ hw_pr2/2.c
@@ -9,15 +9,16 @@ struct Item {
 void main(){
     int n, i;
     scanf("%d", &n);
-    struct Item items[n];
+    struct Item* items = malloc(sizeof(struct Item) * n);
 
     for (i = 0; i < n; i++){
         struct Item it;
-        scanf("%s %d %f", &it.name, &it.count, &it.price);
+        scanf("%s %d %f", &it.name, &it.count, &it.price); // тут можно напрямую в элемент массива вводить, без посредников
         items[i] = it;
     }
 
     for (i = 0; i < n; i++){
         printf("Item: %s\nQuantity: %d\nPrice: %.2f\n\n", items[i].name, items[i].count, items[i].price);
     }
+    free(items);
 }
